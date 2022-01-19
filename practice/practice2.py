295bffd2f46ecf3a4ebcb60bec8e7d48617fbe24
Kimwonjoon <96778880+Kimwonjoon@users.noreply.github.com>
2022-01-19 16:29:22 +0900

    Delete practice directory

--- practice/practice2.py
+++
@@ -1,8 +0,0 @@
-# 참(true) / 거짓(false) bool
-print(5 > 10) # False
-print(5 < 10) # True
-print(True)
-print(False)
-print(not True)
-print(not False)
-print(not (5 > 10))
--- practice/practice25.py
+++
@@ -1,7 +0,0 @@
-# 키워드값
-def profile(name, age, main_lang):
-    print(name, age, main_lang)
-
-profile(name = "유재석", main_lang= "파이썬", age= 20)
-profile(main_lang= "자바", age= 25, name = "김태호")
-# 키워드를 이용해서 값을 넣어주면 순서 상관없이 키워드에 해당하는 값이 알아서 들어가서 호출됨
